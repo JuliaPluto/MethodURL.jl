024dafabc4d67f762498a25aec03f2df647c4f49
adrhill <adrian.hill@mailbox.org>
2024-10-21 20:32:02 +0200

    Add JuliaFormatter tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,20 @@
 using MethodURL
 using Test
-using Aqua
-using JET
+using JuliaFormatter: JuliaFormatter
+using Aqua: Aqua
+using JET: JET
 
-@testset "MethodURL.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(MethodURL)
-    end
-    @testset "Code linting (JET.jl)" begin
-        JET.test_package(MethodURL; target_defined_modules = true)
+@testset verbose = true "MethodURL.jl" begin
+    @testset verbose = true "Linting" begin
+        @testset "Code formatting (JuliaFormatter.jl)" begin
+            @test JuliaFormatter.format(MethodURL; verbose=false, overwrite=false)
+        end
+        @testset "Code quality (Aqua.jl)" begin
+            Aqua.test_all(MethodURL)
+        end
+        @testset "Code linting (JET.jl)" begin
+            JET.test_package(MethodURL; target_defined_modules=true)
+        end
     end
     # Write your tests here.
 end
