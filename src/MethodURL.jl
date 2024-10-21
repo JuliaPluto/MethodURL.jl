ade23ba13309ac218ec81ba97b85cca2028b71e8
Adrian Hill <gh@adrianhill.de>
2024-10-22 01:39:15 +0200

    Initial functionality (#1)

--- src/MethodURL.jl
+++ src/MethodURL.jl
@@ -1,5 +1,104 @@
+# Based on code by Kristoffer Carlsson:
+# https://github.com/JuliaLang/julia/issues/47709#issuecomment-2388629772
+
 module MethodURL
 
-# Write your package code here.
+using Base: PkgId, UUID, Sys, inbase
+using RegistryInstances: reachable_registries, registry_info
 
+export url
+
+function repo_and_path_to_url(repo, version, path, line)
+    repo = chopsuffix(repo, ".git")
+    # TODO: Handle more git forges
+    if startswith(repo, "https://github.com")
+        # https://github.com/owner/Package.jl/blob/v0.1.0/src/foo.jl#L42
+        return join([repo, "blob", "v" * version, path * "#L$line"], "/")
+    elseif startswith(repo, "https://gitlab.com")
+        # https://gitlab.com/owner/Package.jl/-/blob/v0.1.0/src/foo.jl#L42
+        return join([repo, "-", "blob", "v" * version, path * "#L$line"], "/")
+    elseif startswith(repo, "https://git.sr.ht")
+        # https://git.sr.ht/~owner/Package.jl/tree/v0.1.0/item/src/foo.jl#L42
+        return join([repo, "tree", "v" * version, "item", path * "#L$line"], "/")
+    else
+        error("Failed to construct URL for repository $repo.")
+    end
 end
+
+# Find repository in reachable registries by looking up UUID
+function repos_package(uuid::UUID)
+    repos = String[]
+    for reg in reachable_registries()
+        entry = get(reg, uuid, nothing)
+        if entry !== nothing
+            info = registry_info(entry)
+            push!(repos, info.repo)
+        end
+    end
+    return repos
+end
+
+# Return errors instead of `nothing`
+function _uuid(M::Module)
+    uuid = PkgId(M).uuid
+    isnothing(uuid) && error("Failed to find UUID of package $M.")
+    return uuid
+end
+
+# Return errors instead of `nothing`
+function _pkgdir(M::Module)
+    dir = pkgdir(M)
+    isnothing(dir) && error("Failed to find directory of package $M.")
+    return dir
+end
+
+# TODO: is this heuristic sufficient?
+instdlib(pkgdir) = contains(pkgdir, Sys.STDLIB)
+
+# TODO: If package is devved use local path
+# TODO: If package is added by URL, use that
+# TODO: Support monorepos
+function url(m::Method)
+    M = parentmodule(m)
+    file = String(m.file)
+    line = m.line
+
+    urls = String[]
+    if inbase(M)
+        # adapted from https://github.com/JuliaLang/julia/blob/8f5b7ca12ad48c6d740e058312fc8cf2bbe67848/base/methodshow.jl#L382-L388
+        commit = Base.GIT_VERSION_INFO.commit
+        if isempty(commit)
+            url = "https://github.com/JuliaLang/julia/tree/v$VERSION/base/$file#L$line"
+        else
+            url = "https://github.com/JuliaLang/julia/tree/$commit/base/$file#L$line"
+        end
+        push!(urls, url)
+    else
+        uuid = _uuid(M)
+        repos = repos_package(uuid)
+        pkgdir = _pkgdir(M)
+
+        if isempty(repos) && instdlib(pkgdir) # stdlib package
+            package, file = match(r"/stdlib/v(?:.*?)/(.*?)/src/(.*)", file).captures
+            url = "https://github.com/JuliaLang/julia/blob/v$VERSION/stdlib/$package/src/$file#L$line"
+            push!(urls, url)
+        else # external package
+            pkg_splitpath = splitpath(pkgdir)
+            file_splitpath = splitpath(file)
+            while !isempty(pkg_splitpath) && first(pkg_splitpath) == first(file_splitpath)
+                popfirst!(pkg_splitpath)
+                popfirst!(file_splitpath)
+            end
+            local_dir = join(file_splitpath, "/")
+
+            v = string(pkgversion(M))
+            for repo in repos
+                url = repo_and_path_to_url(repo, v, local_dir, line)
+                push!(urls, url)
+            end
+        end
+    end
+    return urls
+end
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,6 @@
 using MethodURL
+
+# Linting tests
 using Test
 using JuliaFormatter: JuliaFormatter
 using Aqua: Aqua
@@ -12,32 +14,112 @@ using ExplicitImports:
     check_all_qualified_accesses_via_owners,
     check_all_qualified_accesses_are_public
 
+# Packages used for testing
+using HTTP: request
+using InteractiveUtils: @which
+
+# Package to test URLs on
+using LinearAlgebra: det
+using Statistics: mean
+using Plots: Plots # has sub-repositories
+using Arxiv: @arXiv_str # hosted on GitLab
+using GPMaxlik: gnll # hosted on sourcehut
+
+function url_exists(url)
+    response = request("GET", url; status_exception=false, redirect=true, retry=true)
+    if 200 ≤ response.status < 400
+        return true
+    else
+        @warn "Failed to request URL" url response.status response
+        return false
+    end
+end
+
 @testset verbose = true "MethodURL.jl" begin
     @testset verbose = true "Linting" begin
-        @testset "Code formatting (JuliaFormatter.jl)" begin
+        @testset "JuliaFormatter.jl" begin
             @test JuliaFormatter.format(MethodURL; verbose=false, overwrite=false)
         end
-        @testset "Code quality (Aqua.jl)" begin
+        @testset "Aqua.jl" begin
             Aqua.test_all(MethodURL)
         end
-        @testset "Code linting (JET.jl)" begin
+        @testset "JET.jl" begin
             JET.test_package(MethodURL; target_defined_modules=true)
         end
 
-        @testset "Code imports (ExplicitImports.jl)" begin
+        @testset "ExplicitImports.jl" begin
             @testset "Improper implicit imports" begin
                 @test isnothing(check_no_implicit_imports(MethodURL))
             end
             @testset "Improper explicit imports" begin
                 @test isnothing(check_no_stale_explicit_imports(MethodURL))
                 @test isnothing(check_all_explicit_imports_via_owners(MethodURL))
-                @test isnothing(check_all_explicit_imports_are_public(MethodURL))
+                @test isnothing(
+                    check_all_explicit_imports_are_public(
+                        MethodURL; ignore=(:PkgId, :UUID, :inbase)
+                    ),
+                )
             end
             @testset "Improper qualified accesses" begin
                 @test isnothing(check_all_qualified_accesses_via_owners(MethodURL))
                 @test isnothing(check_no_self_qualified_accesses(MethodURL))
-                @test isnothing(check_all_qualified_accesses_are_public(MethodURL))
+                @test isnothing(
+                    check_all_qualified_accesses_are_public(
+                        MethodURL; ignore=(:GIT_VERSION_INFO,)
+                    ),
+                )
             end
         end
+    end
+    @testset verbose = true "URL" begin
+        @testset "Base" begin
+            m = @which sqrt(0.0)
+            u = first(@inferred url(m))
+            # @test url_exists(u)
+        end
+        @testset "Stdlib" begin
+            @testset "within julialang/julia" begin
+                m = @which @test true
+                u = first(@inferred url(m))
+                # @test url_exists(u)
+
+                m = @which det(rand(2, 2))
+                u = first(@inferred url(m))
+                # @test url_exists(u)
+            end
+            @testset "own repository" begin
+                m = @which mean(rand(5))
+                u = first(@inferred url(m))
+                # @test url_exists(u)
+            end
+        end
+
+        @testset "External" begin
+            @testset "GitHub" begin
+                m = @which Aqua.test_all(MethodURL)
+                u = first(@inferred url(m))
+                # @test url_exists(u)
+            end
+            @testset "GitHub monorepo" begin
+                m = first(methods(Plots.RecipesBase.create_kw_body))
+                u = first(@inferred url(m))
+                # @test url_exists(u)
+            end
+            @testset "GitLab" begin
+                m = @which arXiv"1234.5678"
+                u = first(@inferred url(m))
+                # @test url_exists(u) # no tags in Arxiv.jl
+            end
+            @testset "Sourcehut" begin
+                m = first(methods(gnll))
+                u = first(@inferred url(m))
+                # @test url_exists(u) # no tags in GPMaxlik.jl
+            end
+        end
+        # @testset "Local" begin
+        #     m = @which url(@which sqrt(1.0))
+        #     u = first(@inferred url(m))
+        #     # @test url_exists(u)
+        # end
     end
 end
